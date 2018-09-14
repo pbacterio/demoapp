ef2f3fe2a6a04f6d1d8e3cab2a5310edd7a1a12c
Germán Moya <pbacterio@gmail.com>
2018-09-14 13:14:49 +0200

    Docker multiarch images

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
-FROM golang:alpine as builder
-RUN apk --no-cache add bash upx;
-RUN wget -q -O - https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-ADD . /go/src/github.com/pbacterio/demoapp/
-WORKDIR /go/src/github.com/pbacterio/demoapp
-RUN ./build.sh
-
 FROM scratch
-COPY --from=builder /go/src/github.com/pbacterio/demoapp/build/linux/demoapp /
+ARG OS
+ARG ARCH
+ADD build/${OS}_${ARCH}/demoapp /demoapp
 CMD ["/demoapp"]
--- build.sh
+++ build.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
+function build_image {
+    docker build --force-rm --build-arg OS=${1} --build-arg ARCH=${2} -t demoapp:${1}-${2} .
+}
+
+pushd "$(dirname "$0")"
+
 if [[ $1 == "clean" ]]
 then
     set -ex
-    rm -r build
+    rm -rf build
     exit
 fi
 
 if [[ $1 == "docker" ]]
 then
     set -ex
-    docker build --force-rm --tag demoapp .
+    build_image linux amd64
+    build_image linux arm
+    build_image linux arm64
+    build_image darwin amd64
     exit
 fi
 
+# function in-docker {
+#     docker run -v $PWD:/go/src/github.com/pbacterio/demoapp \
+#       --rm -it gobuilder "/go/src/github.com/pbacterio/demoapp/build.sh"
+# }
+
+function build {
+    CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -o build/${1}_${2}/demoapp -ldflags="-s -w"
+}
+
 set -ex
 dep ensure
-CGO_ENABLED=0 GOOS=linux go build -o build/linux/demoapp -ldflags="-s -w"
-upx -q build/linux/demoapp
+build linux amd64
+build linux arm
+build linux arm64
+build darwin amd64
+upx -q build/*/*
